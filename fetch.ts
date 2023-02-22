f62552720638ca2e4f7be60d5d2d5974f06f6fc8
Trekusov Max <maxtrekusovsite@gmail.com>
2023-02-22 13:15:16 +0300

    task 4 done

---
+++ fetch.ts
@@ -0,0 +1,17 @@
+interface Comments {
+  postId: number
+  id: number
+  name: string
+  email: string
+  body: string
+}
+
+const COMMENTS_URL: string = 'https://jsonplaceholder.typicode.com/comments'
+
+const getData = (url: string): Promise<Comments[]> => {
+  return fetch(url).then((res) => res.json())
+}
+
+getData(COMMENTS_URL).then((comment) =>
+  comment.forEach((c) => console.log({ ID: c.id, Email: c.email })),
+)
