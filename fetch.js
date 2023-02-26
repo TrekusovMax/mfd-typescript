fdd3788d3b85df8cb40df4407f4d8e5f856bef19
Maxim <maxtrekusovsite@gmail.com>
2023-02-26 18:40:30 +0300

    added eslint

--- fetch.js
+++
@@ -1,9 +0,0 @@
-var COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
-var getData = function (url) {
-    return fetch(url).then(function (res) { return res.json(); });
-};
-getData(COMMENTS_URL).then(function (comment) {
-    return comment.forEach(function (c) {
-        return console.log('ID: ' + c.id.toString() + ', Email: ' + c.email);
-    });
-});
