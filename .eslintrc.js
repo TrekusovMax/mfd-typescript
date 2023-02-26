1c8b76de5ce3ebc3d4d2cc9cffd6d346f1931a1f
Maxim <maxtrekusovsite@gmail.com>
2023-02-26 18:38:19 +0300

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  rules: {
+    'no-unused-vars': 0,
+  },
+}
+
